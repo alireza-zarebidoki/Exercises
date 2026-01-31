e62d272ce4444fa1c6cd7731ab969d55040ad36e
Alireza ZareBidoki <alireza.zarebidoki.1111@gmail.com>
2026-01-31 16:11:56 +0330

    Adding CSES Exercises 3

---
+++ CSES/1-Introductory-Problems/3-Repetitions/main1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    //Variables
+    char in[100001];
+    int temp = 1;
+    int out = 1;
+    int i;
+
+    //Input
+    scanf("%s",in);
+
+    //Calculation
+    for (i = 1; in[i]; i++)
+    {
+        if (in[i] == in[i-1])
+        {
+            temp++;
+            if(temp>= out)out = temp;
+        }
+        else
+        {
+            temp = 1;
+        }
+
+    }
+
+    //Output
+    printf("%d",out);
+
+    return 0;
+}
